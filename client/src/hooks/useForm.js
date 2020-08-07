20118cd166b9d949599534b81a332903175b79e8
Stacey Louis <stacey-louis@lambdastudents.com>
2020-08-07 11:24:07 -0400

    initial commit

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+import React , {useState} from 'react'
+export const useForm = initialValue => {
+    const [showSuccessMessage, setSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue)
+
+    const handleChange = e =>{
+        setValues({...values, [e.target.name]: e.target.value})
+    }
+    const handleSubmit = e =>{
+        e.preventDefault();
+        setSuccessMessage(true)
+    }
+    return [handleChange, handleSubmit, values, showSuccessMessage]
+}
